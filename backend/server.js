f3f116c1b1d0a4c2251dcccb63f2336097bc417b
RamyaUmmineni <ramyaummineni@gmail.com>
2025-11-02 17:28:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL;
 
-app.use(cors({
-  origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
-  credentials: true
-}));
+
 app.use(express.json());
 
 mongoose.connect(MONGO_URL)
